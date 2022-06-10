9468d47a2fbd26b4d0441856ee6653407967d8c2
Alex Yegian <ayegian@ucsc.edu>
2022-06-09 23:14:24 -0700

    still trying to fix cors 30

--- server/index.js
+++ server/index.js
@@ -132,19 +132,18 @@ var corsOptions = {
 
 app.post("/api/post-transcript", cors(corsOptions), (req,res)=>{
     console.log("POSt transcript");
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // );
+    // if (req.method === 'OPTIONS') {
+    //   res.status(200).end()
+    //   return
+    // }
     const form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files){
