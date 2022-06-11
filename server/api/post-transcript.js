0ad9f3f9b8991895b0f1ca708b2114557c1a0a4a
Alex Yegian <ayegian@ucsc.edu>
2022-06-10 17:47:12 -0700

    still trying to fix cors 53

--- server/api/post-transcript.js
+++ server/api/post-transcript.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("mysql");
+const cors = require("cors");
 
-var mydb = mysql.createPool({
-    host: "sql3.freemysqlhosting.net",
-    user: "sql3498610",
-    password: "YhycF8lTEC",
-    database: "sql3498610",
-    connectionLimit: 10
-});
-
-
-router.post("/", async (req, res)=>{
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+router.post("/", cors(corsOptions), async (req, res)=>{
     try{
         console.log("Connected")
         res.send("POST TRANSCRIPT");
