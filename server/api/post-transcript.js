3addf3d9bf4207f4d59864bb6479c7351c5c6565
Alex Yegian <ayegian@ucsc.edu>
2022-06-10 19:35:07 -0700

    still trying to fix cors 57

--- server/api/post-transcript.js
+++ server/api/post-transcript.js
@@ -7,15 +7,37 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 router.post("/", cors(corsOptions), async (req, res)=>{
-    try{
-        console.log("Connected")
-        res.send("POST TRANSCRIPT");
-    }catch(error){
-        console.error(error);
-        res.send("ERROR");
-        return res.status(500).send("Server error");
-    }
+    console.log("POSt transcript");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // );
+    // if (req.method === 'OPTIONS') {
+     //   res.status(200).end()
+    //   return
+    // }
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files){
+        res.send("POST TEST");
+    });
 });
+
+// router.post("/", cors(corsOptions), async (req, res)=>{
+//     try{
+//         console.log("Connected")
+//         res.send("POST TRANSCRIPT");
+//     }catch(error){
+//         console.error(error);
+//         res.send("ERROR");
+//         return res.status(500).send("Server error");
+//     }
+// });
+
 
 module.exports = router;
 // module.exports = (req, res) => {
@@ -24,3 +46,71 @@ module.exports = router;
 //       `This response would create a new team called ${name}, using a POST request.`,
 //     );
 //   };
+
+
+// app.post("/api/post-transcript", cors(corsOptions), (req,res)=>{
+//     console.log("POSt transcript");
+// //     res.setHeader('Access-Control-Allow-Credentials', true);
+// //     res.setHeader('Access-Control-Allow-Origin', '*');
+// //   // another common pattern
+// //   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+// //     // res.setHeader(
+// //     //   'Access-Control-Allow-Headers',
+// //     //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+// //     // );
+// //     // if (req.method === 'OPTIONS') {
+// //     //   res.status(200).end()
+// //     //   return
+// //     // }
+// //     const form = new formidable.IncomingForm();
+
+// //     form.parse(req, function(err, fields, files){
+// //         console.log("FILES");
+// //         console.log(files);
+// //         const src = files.file.filepath;
+// //         const dest = uploadPath+files.file.originalFilename;
+// //         //fs.move(src, dest, { overwrite: true }).then(() => console.log("File moved to the destination"+" folder successfully")).then(() =>{
+// //             //console.log("Start Extract");
+// //             // const pdfExtract = new PDFExtract();
+// //             // const options = {}; /* see below */
+// //             // pdfExtract.extract('./uploads/'+files.file.originalFilename, options, (err, data) => {
+// //             // if (err) {console.log("ERR");console.log(err); return;}
+// //             //     console.log("DATA");
+// //             //     console.log(data.text);
+// //             //     dataToSend = data;
+// //             // });
+// //             // console.log("End extract");
+            
+// //             // pdfUtil.pdfToText("./uploads/"+files.file.originalFilename, function(err, data) {
+// //             //     if (err) throw(err);
+// //             //     console.log("Data:");
+// //             //     console.log(data); //print all text    
+// //             //     var dataToSend = data;
+// //             //     res.send(dataToSend);
+// //             //   });
+// //             //console.log("End Extract");
+
+// //         //});
+
+
+// //         //pdfUtil.pdfToText(src, function(err, data) {
+// //         //    if (err) {throw(err);}
+// //         //     console.log("Data:");
+// //         //     console.log(data); //print all text    
+// //         //     var dataToSend = data;
+// //         //     res.send(dataToSend);
+// //         //});
+// //         res.send("TEST");
+
+// //     //     console.log("SPAWN PYTHON");
+// //         //const python = spawn();
+// //         // const python = spawn('python', ['./pdfTextExtract.py', files.file.originalFilename]);
+// //         //python.stdout.on('data', function (data) {
+// //         //dataToSend = data.toString();
+// //         // });
+// //         //console.log("CLOSE PYTHON");
+// //         //python.on('close', (code) => {
+        
+// //         //});
+// //     })
+// });
