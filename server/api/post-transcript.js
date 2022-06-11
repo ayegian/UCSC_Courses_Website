cea5836bbe8b917330918f524780063e7d2bf66d
Alex Yegian <ayegian@ucsc.edu>
2022-06-10 22:59:38 -0700

    still trying to fix cors 68

--- server/api/post-transcript.js
+++ server/api/post-transcript.js
@@ -33,9 +33,52 @@ router.post("/", cors(corsOptions), async (req, res)=>{
     // }
 
     form.parse(req, function(err, fields, files){
-        res.send("POST TEST");
+        // res.send("POST TEST");
+// //         console.log(files);
+        const src = files.file.filepath;
+        const dest = uploadPath+files.file.originalFilename;
+        //fs.move(src, dest, { overwrite: true }).then(() => console.log("File moved to the destination"+" folder successfully")).then(() =>{
+            //console.log("Start Extract");
+            // const pdfExtract = new PDFExtract();
+            // const options = {}; /* see below */
+            // pdfExtract.extract('./uploads/'+files.file.originalFilename, options, (err, data) => {
+            // if (err) {console.log("ERR");console.log(err); return;}
+            //     console.log("DATA");
+            //     console.log(data.text);
+            //     dataToSend = data;
+            // });
+            // console.log("End extract");
+      
+            // pdfUtil.pdfToText("./uploads/"+files.file.originalFilename, function(err, data) {
+            //     if (err) throw(err);
+            //     console.log("Data:");
+            //     console.log(data); //print all text    
+            //     var dataToSend = data;
+            //     res.send(dataToSend);
+            //   });
+            //console.log("End Extract");
+
+        //});
+        pdfUtil.pdfToText(src, function(err, data) {
+            if (err) {throw(err);}
+            console.log("Data:");
+            console.log(data); //print all text    
+            var dataToSend = data;
+            res.send(dataToSend);
+        });
+        // res.send("TEST");
+
+    //     console.log("SPAWN PYTHON");
+        //const python = spawn();
+        // const python = spawn('python', ['./pdfTextExtract.py', files.file.originalFilename]);
+        //python.stdout.on('data', function (data) {
+        //dataToSend = data.toString();
+        // });
+        //console.log("CLOSE PYTHON");
+        //python.on('close', (code) => {
+        
     });
-    res.send("DERP");
+    // res.send("DERP");
 });
 
 // router.post("/", cors(corsOptions), async (req, res)=>{
